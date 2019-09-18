4c5bb0b998cb08ef18a44233f025727da9923419
Shelby Moses <sm126215@ohio.edu>
2019-09-18 09:53:01 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MIS4200_SM126215.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200_SM126215.Startup))]
+namespace MIS4200_SM126215
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
