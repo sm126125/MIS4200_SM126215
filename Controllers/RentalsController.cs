5d6062a3e5a04777a6a7aadcf3ef20e70d36ef1e
Shelby Moses <sm126215@ohio.edu>
2019-09-22 15:38:56 -0400

    index page

---
+++ Controllers/RentalsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200_SM126215.DAL;
+using MIS4200_SM126215.Models;
+
+namespace MIS4200_SM126215.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Rentals
+        public ActionResult Index()
+        {
+            var rental = db.Rental.Include(r => r.Property).Include(r => r.Student);
+            return View(rental.ToList());
+        }
+
+        // GET: Rentals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rental rental = db.Rental.Find(id);
+            if (rental == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rental);
+        }
+
+        // GET: Rentals/Create
+        public ActionResult Create()
+        {
+            ViewBag.propertyID = new SelectList(db.Property, "propertyID", "address");
+            ViewBag.studentID = new SelectList(db.Student, "studentID", "firstName");
+            return View();
+        }
+
+        // POST: Rentals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RentalID,studentID,propertyID,monthlyRent")] Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rental.Add(rental);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.propertyID = new SelectList(db.Property, "propertyID", "address", rental.propertyID);
+            ViewBag.studentID = new SelectList(db.Student, "studentID", "firstName", rental.studentID);
+            return View(rental);
+        }
+
+        // GET: Rentals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rental rental = db.Rental.Find(id);
+            if (rental == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.propertyID = new SelectList(db.Property, "propertyID", "address", rental.propertyID);
+            ViewBag.studentID = new SelectList(db.Student, "studentID", "firstName", rental.studentID);
+            return View(rental);
+        }
+
+        // POST: Rentals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RentalID,studentID,propertyID,monthlyRent")] Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rental).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.propertyID = new SelectList(db.Property, "propertyID", "address", rental.propertyID);
+            ViewBag.studentID = new SelectList(db.Student, "studentID", "firstName", rental.studentID);
+            return View(rental);
+        }
+
+        // GET: Rentals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rental rental = db.Rental.Find(id);
+            if (rental == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rental);
+        }
+
+        // POST: Rentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rental rental = db.Rental.Find(id);
+            db.Rental.Remove(rental);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
