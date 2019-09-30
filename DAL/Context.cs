91b986dc8a52c37ba9fb7abba1961a46d42f40ae
Shelby Moses <sm126215@ohio.edu>
2019-09-30 13:06:14 -0400

    migration folder

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -11,13 +11,18 @@ namespace MIS4200_SM126215.DAL
     {
         public Context() : base ("name=DefaultConnection")
         {
-
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context,
+            MIS4200_SM126215.Migrations.MISContext.Configuration>("DefaultConnection"));
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Rental> Rentals { get; set; }
         public DbSet<Property> Properties { get; set; }
-        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
